bb8eb75cd1a14eda58487dcc7d3ef698510328d9
shweta2kb <shweta2kb@gmail.com>
2021-02-17 20:15:05 +0000

    Remove this error" Expected 'object' to be 'string'."

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,11 +1,10 @@
 const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 // TODO 1.1a: Define candidateName // 
-let candidateName;
-let count;
+let candidateName='';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question="";
-let correctAnswer='';
+let question = "Who was the first American woman in space? ";
+let correctAnswer;
 let candidateAnswer=[];
 let questions=["Who was the first American woman in space? ",
             "True or false: 5000 meters = 5 kilometers. ",
@@ -20,7 +19,7 @@ let correctAnswers=["Sally Ride",
 let candidateAnswers=[];
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-let info = input.question("Enter condidate name : ");
+  candidateName = input.question("Please Enter your name: ")
 }
 function askQuestion() {
   //  1.2b: Ask candidate the question and assign the response as candidateAnswer //
@@ -38,7 +37,6 @@ function gradeQuiz(candidateAnswers) {
   let i = 0;
   let grade = 0;
   let status = ''
-
   console.log(`Candidate Name: ${candidateName}`);
   while (i < questions.length) {
     
@@ -64,6 +62,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name // 
+  console.log(`Welcome, ${candidateName}`)
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
