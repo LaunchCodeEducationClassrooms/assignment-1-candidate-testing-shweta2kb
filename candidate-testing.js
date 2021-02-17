dfbef7224cb05e394e832852169638bca0467e5f
shweta2kb <shweta2kb@gmail.com>
2021-02-17 20:22:31 +0000

    Remove error " Expected 'object' to be 'string'"

--- candidate-testing.js
+++ candidate-testing.js
@@ -4,8 +4,8 @@ const input = require('readline-sync');
 let candidateName='';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
-let correctAnswer;
-let candidateAnswer=[];
+let correctAnswer = "Sally Ride";
+let candidateAnswer='';
 let questions=["Who was the first American woman in space? ",
             "True or false: 5000 meters = 5 kilometers. ",
             "(5 + 3)/2 * 10 = ? ",
